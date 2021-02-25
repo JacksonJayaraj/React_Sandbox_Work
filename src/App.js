e41682cbaffc2f463c68cdd0ee0197c4396d7023
JacksonJayaraj <54722686+JacksonJayaraj@users.noreply.github.com>
2021-02-25 10:21:45 -0700

    List

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Good Morning </h1>;
+  return (
+    <div>
+      <Header days={days} />
+    </div>
+  );
 }
+
+function Header(props) {
+  return (
+    <div>
+      <h1> React Tutorial</h1>
+      <ul>
+        {props.days.map((day) => (
+          <li> {day} </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+const days = ["Monday", "Tuesday"];
