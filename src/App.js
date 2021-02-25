12606e91d65adb350f7dc928c0fb547d2ac9a4bf
JacksonJayaraj <54722686+JacksonJayaraj@users.noreply.github.com>
2021-02-25 16:02:53 -0700

    API fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
+export default function App({ login }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then((responce) => responce.json())
+      .then(setData);
+  }, []);
+
+  if (data) {
+    return <div> {JSON.stringify(data)} </div>;
+  }
+  return <div>No Data</div>;
+
+  /*
+   return (
     <div>
       <Header days={days} />
+      <Section login={login} />
     </div>
   );
+  */
 }
 
 function Header(props) {
