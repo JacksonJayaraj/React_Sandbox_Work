7ed0a3a7ebd04fdaa41a7d74fd94e278f459b0cd
JacksonJayaraj <54722686+JacksonJayaraj@users.noreply.github.com>
2021-03-04 16:53:53 -0700

    Props-object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import "./styles.css";
 import ContactCard from "./ContactCard";
 
 function App() {
-  return <ContactCard />;
+  return (
+    <div className="contacts">
+      <ContactCard
+        contact={{
+          name: "Jackson",
+          phone: "6028023744",
+          email: "jackson@gmail.com"
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Joanna",
+          phone: "6028023744",
+          email: "joanna41191@gmail.com"
+        }}
+      />
+      <ContactCard
+        contact={{
+          name: "Jayaraj",
+          phone: "123-456-7890",
+          email: "jayaraj@hotmail.com"
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,13 +1,14 @@
 import React from "react";
 //import Jlogo from "./Jlogo.jpg";
 
-function ContactCard() {
+function ContactCard(props) {
+  console.log(props);
   return (
-    <div>
+    <div className="contact-card">
       <img src="http://placekitten.com/300/200" />
-      <h3> Mr.Jackson </h3>
-      <p> Phone: 602-802-3744</p>
-      <p>email : jackson.jayaraj@aexp.com</p>
+      <h3> {props.contact.name} </h3>
+      <p> Phone: {props.contact.phone}</p>
+      <p>email : {props.contact.email}</p>
     </div>
   );
 }
